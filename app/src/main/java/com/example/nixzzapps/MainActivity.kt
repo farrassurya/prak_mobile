b5493ab19c9d3af2ffec75c1dc6460fe120b36e8
farrassurya <suryaputra24si@mahasiswa.pcr.ac.id>
2026-04-08 10:06:03 +0700

    Pertemuan 4 Finish

--- app/src/main/java/com/example/nixzzapps/MainActivity.kt
+++ app/src/main/java/com/example/nixzzapps/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.example.nixzzapps
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.nixzzapps.databinding.ActivityMainBinding
+import com.example.nixzzapps.databinding.ActivityThirdBinding
+import com.example.nixzzapps.pertemuan_4.FourthActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            // Inisialisasi komponen
+
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.btnToFourth.setOnClickListener {
+            val intent = Intent(this, FourthActivity::class.java)
+            startActivity(intent)
+
+            /*tambahkan bagian berikut*/
+            intent.putExtra("nama", "Politeknik Caltex Riau")
+            intent.putExtra("asal", "Rumbai")
+            intent.putExtra("usia", 25)
+
+            startActivity(intent)
         }
     }
 }
